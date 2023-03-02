09e46814dc6ad8bf1504a1e8403d89b6f9ae1ba5
aakksshhaayy4 <akhareakshay2@gmail.com>
2023-03-03 00:09:09 +0530

    changes for signup page

--- src/components/UserManagement.vue
+++ src/components/UserManagement.vue
@@ -1,30 +1,49 @@
 <template>
   <div class="container">
-    <h1 class="text-center">Users List</h1>
-    <table class="table table-stripped">
-      <thead>
-        <tr>
-          <th>User Id</th>
-          <th>User Name</th>
-          <th>Email</th>
-          <th>Phone Number</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="user in users" :key="user.id">
-          <td>{{ user.userid }}</td>
-          <td>{{ user.userName }}</td>
-          <td>{{ user.email }}</td>
-          <td>{{ user.phoneNumber }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <h3 class="text-center mb-4">Register User</h3>
+
+    <div class="w-75 mx-auto">
+      <form class="w-50 mx-auto" v-on:submit.prevent="signUp()">
+        <input
+          type="text"
+          class="form-control mb-3 border border-primary"
+          v-model="username"
+          placeholder="UserName"
+          required
+        />
+        <input
+          type="number"
+          required
+          v-model="phonenumber"
+          placeholder="Phone Number"
+          maxlength="10"
+          minlength="10"
+          class="form-control mb-3 border border-primary"
+        />
+        <input
+          type="email"
+          v-model="email"
+          placeholder="Email"
+          required
+          class="form-control mb-3 border border-primary"
+        />
+        <input
+          type="password"
+          v-model="password"
+          placeholder="Create Password"
+          class="form-control mb-3 border border-primary"
+          required
+        />
+
+        <button type="submit" class="btn btn-outline-primary">Sign Up</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 import UserManagementService from "../services/UserManagementService";
-import axios from 'axios';
+import axios from "axios";
 // import { defineComponent } from "@vue/composition-api";
 
 // export default defineComponent({
@@ -36,17 +55,39 @@ export default {
   data() {
     return {
       users: [],
+      username: "",
+      phonenumber: "",
+      email: "",
+      password: "",
     };
   },
   methods: {
     getAllUsers() {
       axios
         .get(UserManagementService.backend_path + "user/list")
-        .then((res)=>{
-          this.users = res.data
-          console.log(res.data)
-        })
-    }
+        .then((res) => {
+          this.users = res.data;
+          console.log(res.data);
+        });
+    },
+    signUp() {
+      let body = {
+        userName: this.username,
+        password: this.password,
+        phoneNumber: this.phonenumber.toString(),
+        email: this.email,
+      };
+      axios
+        .post(UserManagementService.backend_path + "user/saveorupdate", body)
+        .then((response) => {
+          alert("REcord created Successfull !");
+          console.log(response.data);
+          this.username = ""
+          this.password=""
+          this.phonenumber = ""
+          this.email = ""
+        });
+    },
   },
   mounted() {
     this.getAllUsers();
--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import App from './App.vue'
 import Login from './components/Login/LoginPage.vue';
 import UserManagement from './components/UserManagement.vue';
 import HomePage from './components/Home/HomePage.vue';
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+import  cors from 'cors';
+
 
 const routes = [
     {
@@ -23,4 +28,6 @@ const router = createRouter({
 })
 const app = createApp(App)
 app.use(router)
+app.use(bootstrap)
+app.use(cors)
 app.mount('#app')
